5c4102575e39d368c09016a2c26bc0d8e2dd800b
Kehan Fu <kehanfu@gmail.com>
2021-12-03 18:40:26 +0000

    assingment finished

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,26 @@
 const request = require('request');
 
-const fetcher = function(args) {
-  args = process.argv.slice(2);
-  console.group(args);
-  
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   // console.log(url)
-
-  request(`${url}`, (error, response, body) => {
-    // console.log(body)
+  request(url, (error, response, body) => {
     if (error) {
-      console.log('error', error); //Print the error if one occured
-    } else {
-      const data = JSON.parse(body);
-      // console.log(data)
-      console.log(data[0].description);
+      return callback(error, null); // if there is error in request api, the callback's error references parameter in index, null means theres' no description
+    }
+    const [data] = JSON.parse(body);
+   
+    if (!data) {
+      return callback(null, "Breed not found"); //null references callback's error parameter in index file), "breed not found" references description parameter in index file
     }
     
+    return callback(null, data.description); // no errors, therefore must work
+    
+    //     if (Object.keys(data).length === 0) callback(null, null); => not correct because description dosen't say "breed not found"
+    //  else if (error === null) callback(null, ('Data description is:', data[0].description));
+ 
   });
+   
 };
 
-fetcher();
+
+module.exports = {fetchBreedDescription};
